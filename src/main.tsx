2f71f80adec34baa361cec487458609281999c23
innajain <m.shreyansh.jain@gmail.com>
2024-01-29 13:09:21 +0530

    changed env variable storage type

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
-console.log(JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG))
-// Initialize Firebase
+const firebaseConfig = {
+  "apiKey": import.meta.env.VITE_apiKey,
+  "authDomain": import.meta.env.VITE_authDomain,
+  "projectId": import.meta.env.VITE_projectId,
+  "storageBucket": import.meta.env.VITE_storageBucket,
+  "messagingSenderId": import.meta.env.VITE_messagingSenderId,
+  "appId": import.meta.env.VITE_appId,
+  "measurementId": import.meta.env.VITE_measurementId,
+};
 initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
