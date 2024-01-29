c6bdd11482504dfcdd336cb10090d0f403929e58
innajain <m.shreyansh.jain@gmail.com>
2024-01-29 12:50:02 +0530

    no change

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import Signup from './Pages/Signup'
-import Login from './Pages/Login'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-    
-  return(
+  return (
     <BrowserRouter>
-    <Routes>
-
-    <Route path="/signup" element={<Signup/>} />
-    <Route path="/login" element={<Login/>} />
-    <Route path="/*" element={<div className='w-[100vw] h-[100vh] flex justify-center items-center text-xl font-bold'>Not Found</div>} />
-    </Routes>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/*"
+          element={
+            <div className="w-[100vw] h-[100vh] flex justify-center items-center text-xl font-bold">
+              Not Found
+            </div>
+          }
+        />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
